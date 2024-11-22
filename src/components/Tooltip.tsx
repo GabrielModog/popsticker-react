4325a6d766a4c7c45a17a3426ee53d3669300aae
GabrielModog <modog.dev@gmail.com>
2024-11-22 04:14:25 -0300

    [feat] add tooltip component

---
+++ src/components/Tooltip.tsx
@@ -0,0 +1,33 @@
+import { ReactNode, useState } from "react"
+
+interface TooltipProps {
+  text: string
+  children: ReactNode
+}
+
+export default function Tooltip(props: TooltipProps) {
+  const { children, text } = props
+  const [isHovered, setHovered] = useState(false)
+  function onMouseOver(){
+    setHovered(true)
+  }
+  function onBlur(){
+    setHovered(false)
+  }
+  function onMouseLeave(){
+    setHovered(false)
+  }
+  return (
+    <div 
+    className="tooltip"
+    onBlur={onBlur}
+    onMouseOver={onMouseOver}
+    onMouseLeave={onMouseLeave}
+    >
+      {isHovered && <div className="tooltip__content">
+        {text}
+      </div>}
+      {children}
+    </div>
+  )
+}
